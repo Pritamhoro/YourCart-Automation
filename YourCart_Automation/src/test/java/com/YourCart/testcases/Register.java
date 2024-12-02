76a7dd0402d176b0e9c9eae5c47a0c8f3aac950b
Pritamh <pritam.horo143@gmail.com>
2024-12-02 08:31:20 +0530

    Register Test Cases

--- YourCart_Automation/src/test/java/com/YourCart/testcases/Register.java
+++ YourCart_Automation/src/test/java/com/YourCart/testcases/Register.java
@@ -1,9 +1,25 @@
 package com.YourCart.testcases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Register 
 {
+	WebDriver driver;
+	
+	@Test
 	public void RegisterwithAllMandatoryFiled() 
 	{
-		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://naveenautomationlabs.com/opencart/");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Register']")).click();
 	}
 }
