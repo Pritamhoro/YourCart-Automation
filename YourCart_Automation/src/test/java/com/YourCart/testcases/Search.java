6e73e355563ef99c85e4432fd3593ac2cc566ac0
Pritamh <pritam.horo143@gmail.com>
2024-12-10 08:14:39 +0530

    SearchInValidProduct and validate the message with the assertion.

--- YourCart_Automation/src/test/java/com/YourCart/testcases/Search.java
+++ YourCart_Automation/src/test/java/com/YourCart/testcases/Search.java
@@ -2,6 +2,7 @@ package com.YourCart.testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -28,5 +29,18 @@ public class Search extends base
 	{
 		driver.findElement(By.xpath("//input[@name='search']")).sendKeys("I Phone");
 		driver.findElement(By.xpath("//div[@id='search']/descendant::button")).click();
+		String actualProduct=driver.findElement(By.xpath("//a[contains(text(),'iPhone')]")).getText();
+		Assert.assertEquals(actualProduct, "iPhone","Product is out of stock");
+	}
+	
+	@Test
+	public void SearchInValidProduct() 
+	{
+		driver.findElement(By.xpath("//input[@name='search']")).sendKeys("IQOO");
+		driver.findElement(By.xpath("//div[@id='search']/descendant::button")).click();
+		
+		String actualResult=driver.findElement(By.xpath("//p[contains(text(),'There is no product')]")).getText();
+		String expectedResult="There is no product that matches the search criteria.";
+		Assert.assertEquals(actualResult, expectedResult,"Product is out of stock");
 	}
 }
